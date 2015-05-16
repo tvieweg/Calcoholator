8937e3420d433a72b1b873626676939f40a4adba
Trevor Vieweg <trevor.vieweg@gmail.com>
2015-05-16 14:54:16 -0700

    Completed outlets and actions

---
+++ Calcoholator/Calcoholator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Calcoholator
+//
+//  Created by Trevor Vieweg on 5/16/15.
+//  Copyright (c) 2015 Trevor Vieweg. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
